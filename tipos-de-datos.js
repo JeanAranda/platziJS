0aaa789d29c59750700226bd48212a6f4acec94e
JeanAranda <jarandar2014@gmail.com>
2024-09-12 22:47:18 -0500

    Se sube cuatro archivos de JS basico

---
+++ tipos-de-datos.js
@@ -0,0 +1,27 @@
+//PRIMITIVOS
+//String
+let nombre = "Jean"
+//number
+let edad = 32
+//boolean
+let esMayorDeEdad = true
+//null
+let noHayValor = null
+//undefined
+let noDefinido = undefined //Este valor lo da JS 
+//symbolo
+let simboloUnico = Symbol("único")
+//bigint
+let numeroGrande = 2n
+
+//COMPLEJOS
+//object
+let carro = {
+    marca: "Tesla",
+    modelo: "Model S",
+    precio: "S/. 1000"
+}
+//array
+let frutas = ['manzana', 'banana', 'uvas']
+//function
+function saludar () {}
---
+++ variables.js
@@ -0,0 +1,7 @@
+let notificador = "Jean Pierre"
+console.log(notificador)
+
+//Las variables tienen que ser descriptivas
+let not = "Jean" //No es una buena forma
+//Tambien se utiliza const
+const pi = 3.14 //Es para valores fijos que no cambian

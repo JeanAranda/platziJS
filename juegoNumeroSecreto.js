7a443d920e02459c7adb9fe5f608910ed11119c2
JeanAranda <jarandar2014@gmail.com>
2024-09-27 18:19:27 -0500

    Juego del numero secreto terminado

--- juegoNumeroSecreto.js
+++ juegoNumeroSecreto.js
@@ -2,3 +2,11 @@ const numeroSecreto = Math.floor(Math.random() * 10 +1)
 const numeroJugador = parseInt(prompt("Adivina el numero secreto entre el 1 al 10"))
 
 console.log(`Este es el numero que esta en juego ${numeroJugador}`)
+console.log(`El numero secreto es: ${numeroSecreto}`)
+if (numeroJugador === numeroSecreto){
+    console.log("Felicidades adivinaste el numero secreto!!!!")
+}else if (numeroJugador > numeroSecreto){
+    console.log("El numero secreto es menor")
+}else{
+    console.log("El numero secreto es mayor")
+}

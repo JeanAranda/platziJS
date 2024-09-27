17bc18fab06d877a8c2dce667552677df9299ca9
JeanAranda <jarandar2014@gmail.com>
2024-09-27 18:54:31 -0500

    Se ordena los archivos en carpetas y se termina el ciclo switch

---
+++ ciclos/ejecucion_condicional/switch/switch.js
@@ -0,0 +1,16 @@
+let expr = "Papaya"
+
+switch (expr){
+
+    case "Naranja":
+        console.log("La naranja wando esta S/.5 el Kilo")
+        break
+    case "Platano":
+        console.log("El platano esta barato casero")
+        break
+    case "Papaya":
+        console.log("Se encuentra fuera de estacion")
+        break
+    default:
+        console.log(`Lo siento no contamos con ${expr}`)
+}

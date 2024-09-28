6aea169270d156ea76ba9ef2b0b80e65586cadf0
JeanAranda <jarandar2014@gmail.com>
2024-09-28 00:34:36 -0500

    Se agrega el loop de for of

---
+++ ciclos/loops/for/for-of.js
@@ -0,0 +1,14 @@
+/*
+for - of
+Se utiliza para objetos ietrables
+los objetos iterables son los arrays y string [algo]
+for (variable of objeto){
+    codigo
+}
+
+*/
+let canasta = ["Manzana", "Pera", "Naranja", "Platano"]
+
+for(fruta of canasta){
+    console.log(fruta)
+}
